6c4eaa19d245f95ff653281ef06240093d5d8a84
Alabi Sodiq <adisco4420@gmail.com>
2019-06-13 18:00:32 +0100

    redirect navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Products from './components/routing/products';
 // import Counter from './components/counter-app/counter';
@@ -8,7 +8,8 @@ import Navbar from './components/routing/navbar';
 import Home from './components/routing/home';
 import Dashboard from './components/routing/admin/dashboard';
 import Posts from './components/routing/posts';
-import ProductDetails from './components/routing/productDetails'
+import ProductDetails from './components/routing/productDetails';
+import NotFound from './components/routing/notFound';
 
 function App() {
   return (
@@ -21,8 +22,11 @@ function App() {
         <Route path="/products/:id" component={ProductDetails}/>
         <Route path="/products" render={(props) => <Products sortBy="newest" {...props}/>} />
         <Route path="/posts/:year?/:month?" component={Posts} />
+        <Redirect from="/messages" to="/posts" />
         <Route path="/admin" component={Dashboard} />
-        <Route path="/" component={Home} />
+        <Route path="/not-found" component={NotFound} />
+        <Route path="/" exact component={Home} />
+        <Redirect to="/not-found" />
       </Switch>
     
     </div>
