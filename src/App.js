3b47d2f3dad71a7c0311b90a759f21b19434f1f9
Alabi Sodiq <adisco4420@gmail.com>
2019-06-27 21:28:21 +0100

    update routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Products from './components/routing/products';
@@ -11,27 +11,34 @@ import Posts from './components/routing/posts';
 import ProductDetails from './components/routing/productDetails';
 import NotFound from './components/routing/notFound';
 
-function App() {
-  return (
-    // <Counter />
-    // <HomeVid />
-    <div>
-    <Navbar />
-    <div className="content">
-      <Switch>
-        <Route path="/products/:id" component={ProductDetails}/>
-        <Route path="/products" render={(props) => <Products sortBy="newest" {...props}/>} />
-        <Route path="/posts/:year?/:month?" component={Posts} />
-        <Redirect from="/messages" to="/posts" />
-        <Route path="/admin" component={Dashboard} />
-        <Route path="/not-found" component={NotFound} />
-        <Route path="/" exact component={Home} />
-        <Redirect to="/not-found" />
-      </Switch>
-    
-    </div>
-    </div>
-  );
+class App extends Component {
+  handleNav(conditon) {
+    console.log('nav');
+  }
+  render() {
+    return (
+      // <Counter />
+      // <HomeVid />
+      <div>
+      <Navbar onHandle={this.handleNav} />
+      <div className="content">
+        <Switch>
+          <Route path="/products/:id" component={ProductDetails}/>
+          <Route path="/products" render={(props) => <Products sortBy="newest" {...props}/>} />
+          <Route path="/posts/:year?/:month?" component={Posts} />
+          <Redirect from="/messages" to="/posts" />
+          <Route path="/admin" render={() => <Dashboard onHandle={this.handleNav} />} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/not-found" />
+        </Switch>
+      
+      </div>
+      </div>
+    );
+  }
+  
+
 }
 
 export default App;
--- src/components/routing/navbar.jsx
+++ src/components/routing/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <ul>
       <li>
